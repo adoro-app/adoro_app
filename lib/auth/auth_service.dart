bf3d38928201fc11280485de61891fbe8bfed44b
Moksh Mahajan <mokshmahajan008@gmail.com>
2023-01-16 16:42:18 +0530

    integrated auth apis

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -5,22 +5,62 @@ class AuthService {
 
   AuthService(this._dio);
 
-  // Future login(String mobileNo) async {
-  //   try {
-  //     await _dio.post('/login', data: {'mobileNo' : mobileNo});
-  //   }
-  // }
+  Future<bool> login(String mobileNo) async {
+    try {
+      final response = await _dio.post(
+        '/login',
+        data: {
+          'mobileNo': mobileNo,
+        },
+      );
+      if (response.data['status'] == 200) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
+  }
 
-  Future signUp(String mobileNo, String userName) async {
-    final response = await _dio.post(
-      '/signUp',
-      data: {
-        'mobileNo': mobileNo,
-        'username': userName,
-      },
-    );
-    print(response);
-    print('-----------');
-    print(response.data);
+  Future<bool> signUp(
+    String mobileNo,
+    String username,
+  ) async {
+    try {
+      final response = await _dio.post(
+        '/signup',
+        data: {
+          'mobileNo': mobileNo,
+          'username': username,
+        },
+      );
+      if (response.data['status'] == 200) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> validateOTP(
+    String mobileNo,
+    String otp,
+  ) async {
+    try {
+      final response = await _dio.post(
+        '/validateOTP',
+        data: {
+          'mobileNo': mobileNo,
+          'otp': otp,
+        },
+      );
+      if (response.data['status'] == 200) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
   }
 }
