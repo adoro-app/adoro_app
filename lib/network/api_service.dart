a0da0477162ff3cfab72518b76ccf12c69e96618
Moksh Mahajan <mokshmahajan008@gmail.com>
2023-01-18 15:46:21 +0530

    integrated meme_categories api

---
+++ lib/network/api_service.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:socialv/models/meme_category.dart';
+
+class ApiService {
+  final Dio _dio;
+
+  ApiService(this._dio);
+
+  Future<List<MemeCategory>?> getMemeCategories() async {
+    try {
+      final response = await _dio.get(
+        '/meme_categories',
+      );
+      final status = response.data['status'] as int;
+
+      if (status == 200) {
+        final memeCategories =
+            (response.data['data'] as List<Map<String, dynamic>>)
+                .map((e) => MemeCategory.fromJson(e))
+                .toList();
+        return memeCategories;
+      }
+      return null;
+    } catch (e) {
+      print('---------------------');
+      print('ERROR: e');
+      print('---------------------');
+      return null;
+    }
+  }
+}
